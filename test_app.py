1bb30b125481be05ced12f648c54ddda03cd0c44
Efrei2021 <caroline.diana.azeufack.kenfack@efrei.net>
2020-12-16 10:41:00 +0100

    Update test_app file

--- test_app.py
+++ test_app.py
@@ -17,8 +17,7 @@ class FlaskTest(unittest.TestCase):
 
     def test_b_find_similarTweets(self):
         payload = {
-            # tweet with id=200
-            'search': "Poll numbers are starting to look very good. Leading in Florida @CNN Arizona and big jump in Utah. All numbers rising, national way up. Wow"
+        'search': "Poll numbers are starting to look very good. Leading in Florida @CNN Arizona and big jump in Utah. All numbers rising, national way up. Wow"
         }
 
         response = requests.post('http://localhost:5000', data=payload)
@@ -26,16 +25,14 @@ class FlaskTest(unittest.TestCase):
 
     def test_c_check_if_app_can_handle_1000request_per_minute(self):
         # verifier si l'app peut traiter 1000 requete par minute
-        payload = {  # tweet with id=200
+        payload = {
             'search': "Poll numbers are starting to look very good. Leading in Florida @CNN Arizona and big jump in Utah. All numbers rising, national way up. Wow"
         }
-        a = [*range(1, 1001, 1)]
+        a = list(range(1,1000))
         start_time = time.time()
         for i in a:
             response = requests.post('http://localhost:5000', data=payload)
             self.assertEqual(response.status_code, 200)
-            # vérifier que le retour au niveau du tableau de rank contient id du tweet c'est à dire 200
-            # self.assertEqual(response.content,'id')
         self.assertTrue(time.time() - start_time <= 60)
 
 
